b9f5e2ee99ec820551d0a48124cd4b7f030f5c85
conyguz99 <conyguz99@gmail.com>
2019-11-15 10:00:20 -0300

    Mi segundo commit en proyectoweb

--- flashback/flashback/settings.py
+++ flashback/flashback/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sesiones',
+    'sesiones.apps.SesionesConfig',
 ]
 
 MIDDLEWARE = [
--- flashback/sesiones/views.py
+++ flashback/sesiones/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Tipo, Reserva
 # Create your views here.
+def home(request):
+	num_Reserva = Reserva.objects.all().count()
+
+	num_reserva_lista = Reserva.objects.filter(status__exact='R').count()
+
+	return render(
+		request, 
+		'home.html', 
+		context = {'num_Reserva': num_Reserva, 'num_reserva_lista': num_reserva_lista} 
+    )
